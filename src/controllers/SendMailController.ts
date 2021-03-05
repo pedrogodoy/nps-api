17cdcfc0c9977fb9e9bea931fcae946f17bb8a42
pedrogodoy <pedrogh.godoy12@gmail.com>
2021-03-04 20:15:59 -0400

    Testing email send

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -3,6 +3,7 @@ import { getCustomRepository } from "typeorm";
 import { SurveysRepository } from "../repositories/SurveysRepository";
 import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository";
 import { UsersRepository } from '../repositories/UsersRepository'
+import SendMailService from "../services/SendMailService";
 
 class SendMailController {
 
@@ -25,9 +26,9 @@ class SendMailController {
       });
     }
 
-    const surveyAlreadyExists = await surveysRepository.findOne({id: survey_id});
+    const survey = await surveysRepository.findOne({id: survey_id});
 
-    if(!surveyAlreadyExists) {
+    if(!survey) {
       return res.status(400).json({
         error: "Survey does not exists"
       })
@@ -41,6 +42,9 @@ class SendMailController {
     await surveysUsersRepository.save(surveyUser);
 
     //send email to user
+
+    await SendMailService.execute(email, survey.title, survey.description);
+
     return res.json(surveyUser);
 
   } 
